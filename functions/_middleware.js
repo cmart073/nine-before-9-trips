95e7f3e9dfba40b6fa500c566aa584f34508c0fa
cmart073 <cmart073@users.noreply.github.com>
2025-12-16 19:59:46 -0600

    Add files via upload

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -17,9 +17,30 @@ export async function onRequest(context) {
 function handleAffiliateRedirect(pathname) {
   // Map of affiliate redirect destinations
   const affiliateMap = {
+    // Bandon Dunes
     '/go/bandon-hotels': 'https://www.booking.com/searchresults.html?ss=Bandon+Dunes+Golf+Resort',
     '/go/bandon-flights': 'https://www.google.com/flights?q=flights+to+north+bend+oregon',
     '/go/bandon-car': 'https://www.rentalcars.com/SearchResults.do?city=North+Bend+Oregon',
+    
+    // Scottsdale
+    '/go/scottsdale-hotels': 'https://www.booking.com/searchresults.html?ss=Scottsdale+Arizona',
+    '/go/scottsdale-flights': 'https://www.google.com/flights?q=flights+to+phoenix',
+    '/go/scottsdale-car': 'https://www.rentalcars.com/SearchResults.do?city=Phoenix+Arizona',
+    
+    // Las Vegas
+    '/go/vegas-hotels': 'https://www.booking.com/searchresults.html?ss=Las+Vegas+Nevada',
+    '/go/vegas-flights': 'https://www.google.com/flights?q=flights+to+las+vegas',
+    '/go/vegas-car': 'https://www.rentalcars.com/SearchResults.do?city=Las+Vegas+Nevada',
+    
+    // Palm Springs
+    '/go/palmsprings-hotels': 'https://www.booking.com/searchresults.html?ss=Palm+Springs+California',
+    '/go/palmsprings-flights': 'https://www.google.com/flights?q=flights+to+palm+springs',
+    '/go/palmsprings-car': 'https://www.rentalcars.com/SearchResults.do?city=Palm+Springs+California',
+    
+    // San Diego
+    '/go/sandiego-hotels': 'https://www.booking.com/searchresults.html?ss=Carlsbad+California',
+    '/go/sandiego-flights': 'https://www.google.com/flights?q=flights+to+san+diego',
+    '/go/sandiego-car': 'https://www.rentalcars.com/SearchResults.do?city=San+Diego+California',
   };
 
   const destination = affiliateMap[pathname];
